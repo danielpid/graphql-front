9e7ce5a53de53a1ce26a5f6d8f0a253c76e45c92
danielpid <daniel.pinillos.diaz@gmail.com>
2019-12-08 18:52:59 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import { useQuery, useMutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import PhoneForm from './components/PhoneForm'
 
-function App() {
+const ALL_PERSONS = gql`
+{
+  allPersons  {
+    name
+    phone
+    id
+  }
+}
+`
+
+const CREATE_PERSON = gql`
+  mutation createPerson($name: String!, $street: String!, $city: String!, $phone: String) {
+    addPerson(
+      name: $name,
+      street: $street,
+      city: $city,
+      phone: $phone
+    ) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
+const EDIT_NUMBER = gql`
+mutation editNumber($name: String!, $phone: String!) {
+  editNumber(name: $name, phone: $phone)  {
+    name
+    phone
+    address {
+      street
+      city
+    }
+    id
+  }
+}
+`
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const handleError = (error) => {
+    setErrorMessage(error.graphQLErrors[0].message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  const persons = useQuery(ALL_PERSONS)
+  const [addPerson] = useMutation(CREATE_PERSON, {
+    onError: handleError,
+    refetchQueries: [{ query: ALL_PERSONS }]
+  })
+  const [editNumber] = useMutation(EDIT_NUMBER)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {errorMessage &&
+        <div style={{color: 'red'}}>
+          {errorMessage}
+        </div>
+      }
+      <Persons result={persons} /> 
+      <h2>create new</h2>
+      <PersonForm addPerson={addPerson} />
+      <h2>change number</h2>
+      <PhoneForm editNumber={editNumber} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
